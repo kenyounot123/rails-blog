599f2abc6e76f939078d040f1008608bd0b1d00e
kenyounot123 <kenlu519@gmail.com>
2023-10-13 23:10:01 -0400

    Add create action in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
-
+  
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
